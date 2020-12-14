3bf2cb1482c54cfb2d203b05e7cc8f08240ec8f3
Istvan Nagy <istvan.stv.nagy@gmail.com>
2020-12-14 22:38:46 +0200

    fix project config

--- run/test_run.py
+++ run/test_run.py
@@ -3,6 +3,6 @@ from visu.frame_visu import FrameVisu
 
 if __name__ == '__main__':
     reader = DataReader()
-    frame_data = reader.read_frame(0)
+    frame_data = reader.read_frame(60)
     visu = FrameVisu(frame_data)
     visu.show()
